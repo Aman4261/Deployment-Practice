553ba8bfcd08d239c31427bb9a89776186e0b4ac
Aman Asif <aman.asif@adglobal360.com>
2025-05-29 17:05:37 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+// server.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello from Render CI/CD!'));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
